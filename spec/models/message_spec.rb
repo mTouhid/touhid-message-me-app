e44f10ecd29529ed5e1451201cab465d9b836b8c
Touhidul Islam <touhidul.islam@justice.gov.uk>
2021-12-24 13:50:35 +0000

    update rspec tests

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "does not create a message when provided empty string" do
+    message = Message.new(body: "")
+
+    expect(message).not_to be_valid  
+  end
+
+  it "creates a message when provided a string" do
+    user = User.create(username: "Test User", password: "password")
+    message = Message.new(body: "Hi, this is a test message", user: user)
+
+    expect(message).to be_valid  
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "doest not create a user, when provided empty strings" do
+    user = User.new(username: "", password: "")
+
+    expect(user).not_to be_valid  
+  end
+
+  it "creates a user, when provided strings" do
+    user = User.new(username: "Test User", password: "password")
+
+    expect(user).to be_valid  
+  end
 end
